fd254d1ca8a22e57346d314f404e7a83bd5abcfa
tomer7493 <tomer7493@gmail.com>
2022-12-31 14:56:35 +0200

    first successful ui run

--- uiRunner.py
+++ uiRunner.py
@@ -1,5 +1,5 @@
-import output
-import PyQt6.QtWidgets
+import uiFile
+import PyQt5.QtWidgets
 
 # Only needed for access to command line arguments
 import sys
@@ -7,9 +7,9 @@ import sys
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 # If you know you won't use command line arguments QApplication([]) works too.
-app = PyQt6.QtWidgets.QApplication(sys.argv)
-ex = output.Ui_MainWindow()
-w = PyQt6.QtWidgets.QMainWindow()
+app = PyQt5.QtWidgets.QApplication(sys.argv)
+ex = uiFile.Ui_MainWindow()
+w = PyQt5.QtWidgets.QMainWindow()
 ex.setupUi(w)
 w.show()
 sys.exit(app.exec())
