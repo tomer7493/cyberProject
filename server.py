06621719b441c6a669943ddfce55fb5edd8b42e9
tomer7493 <tomer7493@gmail.com>
2023-05-02 20:44:39 +0300

    keys added

--- server.py
+++ server.py
@@ -12,8 +12,8 @@ import uiFile
 from finals import *
 from vidstream import ScreenShareClient, StreamingServer
 
-certfile=r"cyberProject\try\localhost.pem"
-cafile = r"cyberProject\try\cacert.pem"
+certfile=r"keys\localhost.pem"
+cafile = r"keys\cacert.pem"
 purpose = ssl.Purpose.CLIENT_AUTH
 context = ssl.create_default_context(purpose, cafile=cafile)
 context.load_cert_chain(certfile)
