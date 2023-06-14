f02ec00938165ae76267fbf84b44a53ca67e8207
tomer7493 <tomer7493@gmail.com>
2023-06-14 13:06:42 +0300

    final editing for the test

--- try_mac.py
+++
@@ -1,4 +0,0 @@
-import uuid
-mac_address = uuid.getnode()
-mac_address_hex = ':'.join(['{:02x}'.format((mac_address >> elements) & 0xff) for elements in range(0,8*6,8)][::-1])
-print(mac_address_hex)
